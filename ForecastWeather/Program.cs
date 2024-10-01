e2136e7c9408a7447553af51108f43276d947252
serhiikorol <korol_sergey@me.com>
2024-10-01 15:19:01 +0300

    Initial commit

---
+++ ForecastWeather/Program.cs
@@ -0,0 +1,121 @@
+﻿using System.Text;
+using System.Text.Json;
+
+namespace ForecastWeather;
+
+internal static class Program
+{
+    private static async Task Main(string[] args)
+    {
+        var city = GetArgumentValue(args, "--city");
+        var weatherApiKey = GetArgumentValue(args, "--weather-api-key");
+        var templateFilePath = GetArgumentValue(args, "--template-file");
+        var outputFilePath = GetArgumentValue(args, "--out-file");
+        var days = int.TryParse(GetArgumentValue(args, "--days"), out var parsedDays) ? parsedDays : 3;
+
+        if (string.IsNullOrEmpty(city) || string.IsNullOrEmpty(weatherApiKey) || string.IsNullOrEmpty(templateFilePath) || string.IsNullOrEmpty(outputFilePath))
+        {
+            Console.WriteLine("Missing required arguments: --city, --weather-api-key, --template-file, --out-file");
+            return;
+        }
+
+        await FetchWeatherForecast(city, weatherApiKey, templateFilePath, outputFilePath, days);
+    }
+
+    private static string? GetArgumentValue(string[] args, string key)
+    {
+        return (from arg in args where arg.StartsWith(key) select arg.Split('=')[1].Trim('\'')).FirstOrDefault();
+    }
+
+    private static async Task FetchWeatherForecast(string city, string weatherApiKey, string? templateFilePath, string? outputFilePath, int days)
+    {
+        var url = $"https://api.weatherapi.com/v1/forecast.json?key={weatherApiKey}&q={city}&days={days}";
+
+        using var client = new HttpClient();
+        var response = await client.GetAsync(url);
+        if (response.IsSuccessStatusCode)
+        {
+            var weatherJson = await response.Content.ReadAsStringAsync();
+            var weatherData = JsonSerializer.Deserialize<WeatherApiResponse>(weatherJson);
+
+            if (weatherData != null)
+            {
+                var todayWeather = weatherData.Forecast.Forecastday[0];
+                var updatedContent = await GenerateWeatherReport(todayWeather, weatherData.Location, templateFilePath);
+
+                if (outputFilePath != null) await File.WriteAllTextAsync(outputFilePath, updatedContent);
+                Console.WriteLine($"Weather report created at {outputFilePath}");
+            }
+            else
+            {
+                Console.WriteLine("No weather data found.");
+            }
+        }
+        else
+        {
+            Console.WriteLine($"Failed to fetch the weather data. Status Code: {response.StatusCode}");
+        }
+    }
+
+    private static async Task<string> GenerateWeatherReport(Forecastday todayWeather, Location location, string? templateFilePath)
+    {
+        var templateContent = await File.ReadAllTextAsync(templateFilePath);
+        var weatherTableContent = GenerateHourlyWeatherTable(todayWeather.Hour);
+
+        var updatedContent = templateContent
+            .Replace("{{ City }}", location.Name)
+            .Replace("{{ Country }}", location.Country)
+            .Replace("{{ TodayWeatherDate }}", todayWeather.Date.ToString("yyyy-MM-dd"))
+            .Replace("{{ TodayWeatherIcon }}", todayWeather.Day.Condition.Icon)
+            .Replace("{{ TodayWeatherCondition }}", todayWeather.Day.Condition.Text)
+            .Replace("{{ TodayWeatherConditionIcon }}", todayWeather.Day.Condition.Icon)
+            .Replace("{{ WeathersTable }}", weatherTableContent)
+            .Replace("{{ UpdatedDateTime }}", DateTime.UtcNow.ToString("yyyy-MM-ddTHH:mm:ssZ"));
+
+        return updatedContent;
+    }
+
+    private static string GenerateHourlyWeatherTable(Current[] hourlyWeathers)
+    {
+        var stringBuilder = new StringBuilder();
+        stringBuilder.AppendLine("<table>");
+        stringBuilder.AppendLine("<tr><th>Hour</th>");
+
+        foreach (var hour in hourlyWeathers)
+        {
+            stringBuilder.AppendLine($"<td>{hour.Time.Split(' ')[1]}</td>");
+        }
+        stringBuilder.AppendLine("</tr>");
+
+        stringBuilder.AppendLine("<tr><th>Weather</th>");
+        foreach (var hour in hourlyWeathers)
+        {
+            stringBuilder.AppendLine($"<td><img src=\"{hour.Condition.Icon}\" alt=\"Weather Icon\"></td>");
+        }
+        stringBuilder.AppendLine("</tr>");
+
+        stringBuilder.AppendLine("<tr><th>Condition</th>");
+        foreach (var hour in hourlyWeathers)
+        {
+            stringBuilder.AppendLine($"<td>{hour.Condition.Text}</td>");
+        }
+        stringBuilder.AppendLine("</tr>");
+
+        stringBuilder.AppendLine("<tr><th>Temperature</th>");
+        foreach (var hour in hourlyWeathers)
+        {
+            stringBuilder.AppendLine($"<td>{hour.TempC} °C</td>");
+        }
+        stringBuilder.AppendLine("</tr>");
+
+        stringBuilder.AppendLine("<tr><th>Wind</th>");
+        foreach (var hour in hourlyWeathers)
+        {
+            stringBuilder.AppendLine($"<td>{hour.WindKph} kph</td>");
+        }
+        stringBuilder.AppendLine("</tr>");
+
+        stringBuilder.AppendLine("</table>");
+        return stringBuilder.ToString();
+    }
+}
---
+++ ForecastWeather/WeatherApiResponse.cs
@@ -0,0 +1,279 @@
+using System.Text.Json.Serialization;
+
+namespace ForecastWeather;
+
+public record WeatherApiResponse
+{
+    [JsonPropertyName("location")]
+    public Location Location { get; set; }
+
+    [JsonPropertyName("current")]
+    public Current Current { get; set; }
+
+    [JsonPropertyName("forecast")]
+    public Forecast Forecast { get; set; }
+}
+
+public record Current
+{
+    [JsonPropertyName("last_updated_epoch")]
+    public double? LastUpdatedEpoch { get; set; }
+
+    [JsonPropertyName("last_updated")]
+    public string LastUpdated { get; set; }
+
+    [JsonPropertyName("temp_c")]
+    public double TempC { get; set; }
+
+    [JsonPropertyName("temp_f")]
+    public double TempF { get; set; }
+
+    [JsonPropertyName("is_day")]
+    public double IsDay { get; set; }
+
+    [JsonPropertyName("condition")]
+    public Condition Condition { get; set; }
+
+    [JsonPropertyName("wind_mph")]
+    public double WindMph { get; set; }
+
+    [JsonPropertyName("wind_kph")]
+    public double WindKph { get; set; }
+
+    [JsonPropertyName("wind_degree")]
+    public double WindDegree { get; set; }
+
+    [JsonPropertyName("wind_dir")]
+    public string WindDir { get; set; }
+
+    [JsonPropertyName("pressure_mb")]
+    public double PressureMb { get; set; }
+
+    [JsonPropertyName("pressure_in")]
+    public double PressureIn { get; set; }
+
+    [JsonPropertyName("precip_mm")]
+    public double PrecipMm { get; set; }
+
+    [JsonPropertyName("precip_in")]
+    public double PrecipIn { get; set; }
+
+    [JsonPropertyName("humidity")]
+    public double Humidity { get; set; }
+
+    [JsonPropertyName("cloud")]
+    public double Cloud { get; set; }
+
+    [JsonPropertyName("feelslike_c")]
+    public double FeelslikeC { get; set; }
+
+    [JsonPropertyName("feelslike_f")]
+    public double FeelslikeF { get; set; }
+
+    [JsonPropertyName("windchill_c")]
+    public double WindchillC { get; set; }
+
+    [JsonPropertyName("windchill_f")]
+    public double WindchillF { get; set; }
+
+    [JsonPropertyName("heatindex_c")]
+    public double HeatindexC { get; set; }
+
+    [JsonPropertyName("heatindex_f")]
+    public double HeatindexF { get; set; }
+
+    [JsonPropertyName("dewpoint_c")]
+    public double DewpointC { get; set; }
+
+    [JsonPropertyName("dewpoint_f")]
+    public double DewpointF { get; set; }
+
+    [JsonPropertyName("vis_km")]
+    public double VisKm { get; set; }
+
+    [JsonPropertyName("vis_miles")]
+    public double VisMiles { get; set; }
+
+    [JsonPropertyName("uv")]
+    public double Uv { get; set; }
+
+    [JsonPropertyName("gust_mph")]
+    public double GustMph { get; set; }
+
+    [JsonPropertyName("gust_kph")]
+    public double GustKph { get; set; }
+
+    [JsonPropertyName("time_epoch")]
+    public double? TimeEpoch { get; set; }
+
+    [JsonPropertyName("time")]
+    public string Time { get; set; }
+
+    [JsonPropertyName("snow_cm")]
+    public double? SnowCm { get; set; }
+
+    [JsonPropertyName("will_it_rain")]
+    public double? WillItRain { get; set; }
+
+    [JsonPropertyName("chance_of_rain")]
+    public double? ChanceOfRain { get; set; }
+
+    [JsonPropertyName("will_it_snow")]
+    public double? WillItSnow { get; set; }
+
+    [JsonPropertyName("chance_of_snow")]
+    public double? ChanceOfSnow { get; set; }
+}
+
+public record Condition
+{
+    [JsonPropertyName("text")]
+    public string Text { get; set; }
+
+    [JsonPropertyName("icon")]
+    public string Icon { get; set; }
+
+    [JsonPropertyName("code")]
+    public double Code { get; set; }
+}
+
+public record Forecast
+{
+    [JsonPropertyName("forecastday")]
+    public Forecastday[] Forecastday { get; set; }
+}
+
+public record Forecastday
+{
+    [JsonPropertyName("date")]
+    public DateTimeOffset Date { get; set; }
+
+    [JsonPropertyName("date_epoch")]
+    public double DateEpoch { get; set; }
+
+    [JsonPropertyName("day")]
+    public Day Day { get; set; }
+
+    [JsonPropertyName("astro")]
+    public Astro Astro { get; set; }
+
+    [JsonPropertyName("hour")]
+    public Current[] Hour { get; set; }
+}
+
+public record Astro
+{
+    [JsonPropertyName("sunrise")]
+    public string Sunrise { get; set; }
+
+    [JsonPropertyName("sunset")]
+    public string Sunset { get; set; }
+
+    [JsonPropertyName("moonrise")]
+    public string Moonrise { get; set; }
+
+    [JsonPropertyName("moonset")]
+    public string Moonset { get; set; }
+
+    [JsonPropertyName("moon_phase")]
+    public string MoonPhase { get; set; }
+
+    [JsonPropertyName("moon_illumination")]
+    public double MoonIllumination { get; set; }
+
+    [JsonPropertyName("is_moon_up")]
+    public double IsMoonUp { get; set; }
+
+    [JsonPropertyName("is_sun_up")]
+    public double IsSunUp { get; set; }
+}
+
+public record Day
+{
+    [JsonPropertyName("maxtemp_c")]
+    public double MaxtempC { get; set; }
+
+    [JsonPropertyName("maxtemp_f")]
+    public double MaxtempF { get; set; }
+
+    [JsonPropertyName("mintemp_c")]
+    public double MintempC { get; set; }
+
+    [JsonPropertyName("mintemp_f")]
+    public double MintempF { get; set; }
+
+    [JsonPropertyName("avgtemp_c")]
+    public double AvgtempC { get; set; }
+
+    [JsonPropertyName("avgtemp_f")]
+    public double AvgtempF { get; set; }
+
+    [JsonPropertyName("maxwind_mph")]
+    public double MaxwindMph { get; set; }
+
+    [JsonPropertyName("maxwind_kph")]
+    public double MaxwindKph { get; set; }
+
+    [JsonPropertyName("totalprecip_mm")]
+    public double TotalprecipMm { get; set; }
+
+    [JsonPropertyName("totalprecip_in")]
+    public double TotalprecipIn { get; set; }
+
+    [JsonPropertyName("totalsnow_cm")]
+    public double TotalsnowCm { get; set; }
+
+    [JsonPropertyName("avgvis_km")]
+    public double AvgvisKm { get; set; }
+
+    [JsonPropertyName("avgvis_miles")]
+    public double AvgvisMiles { get; set; }
+
+    [JsonPropertyName("avghumidity")]
+    public double Avghumidity { get; set; }
+
+    [JsonPropertyName("daily_will_it_rain")]
+    public double DailyWillItRain { get; set; }
+
+    [JsonPropertyName("daily_chance_of_rain")]
+    public double DailyChanceOfRain { get; set; }
+
+    [JsonPropertyName("daily_will_it_snow")]
+    public double DailyWillItSnow { get; set; }
+
+    [JsonPropertyName("daily_chance_of_snow")]
+    public double DailyChanceOfSnow { get; set; }
+
+    [JsonPropertyName("condition")]
+    public Condition Condition { get; set; }
+
+    [JsonPropertyName("uv")]
+    public double Uv { get; set; }
+}
+
+public record Location
+{
+    [JsonPropertyName("name")]
+    public string Name { get; set; }
+
+    [JsonPropertyName("region")]
+    public string Region { get; set; }
+
+    [JsonPropertyName("country")]
+    public string Country { get; set; }
+
+    [JsonPropertyName("lat")]
+    public double Lat { get; set; }
+
+    [JsonPropertyName("lon")]
+    public double Lon { get; set; }
+
+    [JsonPropertyName("tz_id")]
+    public string TzId { get; set; }
+
+    [JsonPropertyName("localtime_epoch")]
+    public double LocaltimeEpoch { get; set; }
+
+    [JsonPropertyName("localtime")]
+    public string Localtime { get; set; }
+}
